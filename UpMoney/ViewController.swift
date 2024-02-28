9e86c3019fcc604682eed266f47b4425af55ff2a
dulingleb <dulingleb@gmail.com>
2024-02-28 22:23:35 +0100

    Initial Commit

---
+++ UpMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpMoney
+//
+//  Created by Dulin Gleb on 28.2.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
